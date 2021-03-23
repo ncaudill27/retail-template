e9ded4c8f920ebd6421c7d8dc65813d0e4105112
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-03-23 16:49:44 -0400

    add styling for ProductCard

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,36 +1,36 @@
 import React, { useState, useRef, useEffect } from "react"
-import styled from 'styled-components'
-import { formatPrice } from '../utils/helpers'
+import styled from "styled-components"
+import { formatPrice } from "../utils/helpers"
 
-const ProductCard = (props) => {
+const ProductCard = props => {
   const {
     currency,
     unit_amount,
-    product: {
-      name,
-      images,
-      description
-    }
+    product: { name, images, description },
   } = props
 
   const imageEl = useRef()
+  const copyEl = useRef()
   const [gridRowEnd, setGridRowEnd] = useState(`span 10`)
 
   useEffect(() => {
-      const height = imageEl.current.getBoundingClientRect().height
-      const rowEnd = Math.floor((height / 20) + 5)
-      setGridRowEnd(`span ${rowEnd}`)
-  }, [imageEl]) //TODO add useWidth hook to dependencies
+    const imgHeight = imageEl.current.getBoundingClientRect().height
+    const copyHeight = copyEl.current.getBoundingClientRect().height
+    const rowEnd = Math.floor((imgHeight / 20) + (copyHeight / 20) + 2)
+    setGridRowEnd(`span ${rowEnd}`)
+  }, [imageEl, copyEl]) //TODO add useWidth hook to dependencies
   // useWidth hook allows for element to be properly placed after window resizing
   // e.g. when a phone gets turned sideways
 
   return (
     <StyledCard span={gridRowEnd}>
-      <div ref={imageEl}>
+      <ImgContainer ref={imageEl}>
         <img src={images.find(Boolean)} alt={description} />
+      </ImgContainer>
+      <div ref={copyEl}>
+        <CardTitle>{name}</CardTitle>
+        <CardPrice>{formatPrice(unit_amount, currency)}</CardPrice>
       </div>
-      <CardTitle>{name}</CardTitle>
-      <CardPrice>{formatPrice(unit_amount, currency)}</CardPrice>
     </StyledCard>
   )
 }
@@ -41,6 +41,11 @@ const StyledCard = styled.div`
   grid-row-end: ${p => p.span};
 `
 
+const ImgContainer = styled.div`
+  width: fit-content;
+  margin: 0 auto;
+`
+
 const CardTitle = styled.h2`
   margin: 0 0 0.5rem;
 `
@@ -48,6 +53,5 @@ const CardTitle = styled.h2`
 const CardPrice = styled.p`
   margin: 0;
 `
-
 
 export default ProductCard
